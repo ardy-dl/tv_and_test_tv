7b9e9967d2b08292e6bb33a4b51bcd86385955be
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-05-16 18:35:11 +0800

    Program fulfills the requirements of the activity

--- test_driver.py
+++ test_driver.py
@@ -27,14 +27,26 @@ class TV:
     # set a new channel    
     def set_channel(self,  new_channel):
         self.channel = new_channel
+        print("New channel has been set:", new_channel)
     # get volume level
     def get_volume(self):
         print(self.name + "'s volume level is " + str(self.volume))
     # set a new volume level
+    def set_volume(self,  new_volume):
+        self.volume = new_volume
+        print("New volume level has been set:", new_volume)
     # increase channel  
+    def channel_up(self):
+        self.channel += 1
     # decrease channel
+    def channel_down(self):
+        self.channel -= 1
     # increase volume
+    def volume_up(self):
+        self.volume += 1
     # decrease volume  
+    def volume_down(self):
+        self.volume -= 1
 # create object of a class
 default_tv = TV()
 tv1 = TV("tv1", 30, 3)
@@ -48,3 +60,7 @@ default_tv.status()
 default_tv.get_channel()
 default_tv.set_channel(122)
 default_tv.show()
+tv2.channel_up()
+tv2.show()
+tv1.volume_down()
+tv1.show()
