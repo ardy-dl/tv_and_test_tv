6313a9f16a6c18119f68ca79c89f497e3fe32659
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-05-16 17:14:36 +0800

    redo the program, methods now working

--- test_driver.py
+++ test_driver.py
@@ -1,19 +1,13 @@
 # create class
 class TV:
     # make the test driver (TestTV)
-    def TestTV(self, channel, volume):
+    def __init__(self, channel, volume):
         self.channel = channel
         self.volume = volume
-    # create functions (1 to 120 channels)
-    def channel(self):
-        for i in range(1, 121):
-            return i
-    # volume (1-7)
-    def volume(self):
-        for i in range(1, 8):
-            return i
-    # on/off
-    # create object of a class
-    tv1 = TV(30, 3)
+
+    def show(self):
+        print(self.channel, self.volume)  
+# create object of a class
+tv1 = TV(30, 3)
     # call the method
-    tv1.channel()
+tv1.show()
