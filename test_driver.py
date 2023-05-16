386cb42d73bc58d2ad72353c02f0e657f3aafbfd
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-05-16 17:32:39 +0800

    fixed the printing format

--- test_driver.py
+++ test_driver.py
@@ -1,13 +1,14 @@
 # create class
 class TV:
     # make the test driver (TestTV)
-    def __init__(self, channel, volume):
+    def __init__(self, name, channel, volume):
+        self.name = name
         self.channel = channel
         self.volume = volume
 
     def show(self):
-        print(self.channel, self.volume)  
+        print(self.name + "'s channel is " + str(self.channel) + " and volume level is " + str(self.volume))  
 # create object of a class
-tv1 = TV(30, 3)
+tv1 = TV("tv1", 30, 3)
     # call the method
 tv1.show()
